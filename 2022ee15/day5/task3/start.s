9bbb6a684293d8a8a3427854684966b1e245101b
Umer Shahid <umer.shahid@10xengineers.ai>
2025-09-09 16:09:23 +0500

    Update start.s

--- 2022ee15/day5/task3/start.s
+++ 2022ee15/day5/task3/start.s
@@ -5,7 +5,7 @@ _start:
     li a0, 0         # Return code 0
     li t0, 1
     la t1, tohost
-    sd t0, (t1)      # Signal Spike to exit
+    sd t0, 0(t1)      # Signal Spike to exit
 1:  j 1b             # Infinite loop if exit fails
 
 .section .tohost
